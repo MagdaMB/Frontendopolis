be99b5bcd12a88ee58cdbd751693f5df52842f09
MagdaMB <magda.m.badura@gmail.com>
2018-01-04 14:01:52 +0100

    next commit

--- zadanie 26/script.js
+++ zadanie 26/script.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function (){
 
 		button2.addEventListener('click', function() {
 			
-			var val[]= new  = document.querySelectorAll("input.wartosc");
+			var val = document.getElementsByClassName("wartosc").val;
 			console.log(val);
 
 
